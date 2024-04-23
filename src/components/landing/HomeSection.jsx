66b295210eb3e2f04e3dc2435821421acd0994e6
JeffreyJoel <jowoloko@gmail.com>
2024-04-23 19:34:59 +0100

    remove text

--- src/components/landing/HomeSection.jsx
+++ src/components/landing/HomeSection.jsx
@@ -14,19 +14,6 @@ export const HomeSection = () => {
 
         {/* Mixer */}
         <Mixer />
-
-        <div className="mx-auto  max-w-screen-4xl px-4 pt-8 xl:gap-0">
-          <div className="mx-auto place-self-center ">
-            <p className="max-w-5xl text-center mx-auto font-mono font-light text-gray-500 text-lg md:text-xl">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry&apos;s standard dummy
-              text ever since the 1500s, when an unknown printer took a galley
-              of
-            </p>
-
-            <div className="mb-4 space-y-4 sm:flex sm:space-x-4 sm:space-y-0"></div>
-          </div>
-        </div>
       </section>
     </>
   );
