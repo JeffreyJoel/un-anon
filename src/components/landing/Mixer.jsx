123995e4ddfc18f1b26b786e074bf8c3dbd28270
JeffreyJoel <jowoloko@gmail.com>
2024-04-23 03:09:19 +0100

    update place holder

--- src/components/landing/Mixer.jsx
+++ src/components/landing/Mixer.jsx
@@ -32,7 +32,7 @@ export const Mixer = () => {
               type="text"
               id="first_name"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 py-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="John"
+              placeholder="0x000000000000000000000000000000"
               onChange={setTxId}
               required
             />
